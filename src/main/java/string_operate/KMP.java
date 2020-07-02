a7620f81f287ca1569daa3ca1b6f6cae25d54fa2
黄永宗 <huangyz@yunjiglobal.com>
2020-07-02 09:52:22 +0800

    新增kmp算法

---
+++ src/main/java/string_operate/KMP.java
@@ -0,0 +1,15 @@
+package string_operate;
+/**
+ * @author 善泉 
+ */
+public class KMP {
+
+	public static void main(String[] args){
+	    String s1 = "wrwrwerwerewrewrewrrwerewtetfdgfdhtrytruygfdhgfdg";
+	    String s2 = "ruyg";
+
+
+
+	}
+
+}
